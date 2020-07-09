5e25b6c419a7fe93daacb7db72392213dfda4ed2
Lorenzo McMaster <lomcmast@c5r3s10.wethinkcode.co.za>
2020-07-09 14:45:41 +0200

    finshed ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomcmast <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 13:44:22 by lomcmast          #+#    #+#             */
+/*   Updated: 2020/07/09 14:34:49 by lomcmast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+void ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+	letter = 'z';
+
+	while(letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter --;
+	}
+}
