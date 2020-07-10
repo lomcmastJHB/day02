9bf6198fbf4f117e107aac4076d805f9d4f0b463
Lorenzo McMaster <lomcmast@c5r3s4.wethinkcode.co.za>
2020-07-10 10:13:45 +0200

    fixed ex02

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: lomcmast <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 13:59:00 by lomcmast          #+#    #+#             */
-/*   Updated: 2020/07/10 08:34:39 by lomcmast         ###   ########.fr       */
+/*   Updated: 2020/07/10 10:10:57 by lomcmast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 void	ft_putchar(char c);
@@ -16,7 +16,7 @@ void	ft_print_numbers(void)
 	char	num;
 
 	num = '0';
-	while(num <= '9')
+	while (num <= '9')
 	{
 		ft_putchar(num);
 		++num;
